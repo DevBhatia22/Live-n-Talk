3ed6c6677a2922e559da4f3dafb3d8ef35f9e03d
someone <a8265999139@gmail.com>
2024-12-28 18:07:15 +0530

    feat(DataBase) : SetUp of DataBase

---
+++ BackEnd/src/lib/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(connect.connection.host);
+    } catch (error) {
+        console.error(error);
+    }
+};
