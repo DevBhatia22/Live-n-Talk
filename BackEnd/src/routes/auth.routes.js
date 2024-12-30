d7cb5a5fbd65fc546bd0ac218348c112c84ad3e5
Dev <devbhatia222004@gmail.com>
2024-12-30 11:36:43 +0530

    fix(protectRoute): Fixed bug on protectRoute Reference

--- BackEnd/src/routes/auth.routes.js
+++ BackEnd/src/routes/auth.routes.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { check, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
-import { protectRouter } from "../middleware/auth.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post('/signUp', signup);
 router.post('/logIn', login);
 router.post('/logOut', logout);
-router.put('/update-profile', protectRouter, updateProfile);
-router.get('check', protectRouter, check);
+router.put('/update-profile', protectRoute, updateProfile);
+router.get('check', protectRoute, check);
 
 export default router;
+
+//677016f79cce186c409f2e3e
+//677235157334c68f7a1a5e20
